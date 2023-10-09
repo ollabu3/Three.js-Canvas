853e0ec5635b4fcf229ed77259415408257901b1
ollabu3 <ollabu3@gmail.com3>
2023-10-09 23:23:16 +0900

    canvas animate particles

---
+++ canvas/animate particles/index.js
@@ -0,0 +1,101 @@
+/*
+
+모니터들이 각각 해상도가 달라 주사율에 따라 requestAnimationFrame이 다르지만
+우리가 정하는 fps로 1초에 몇번 코드를 실행시킬지 정해 now, then값으 차이를 통해 모든 모니터에서 동일하게 animation을 찍어낼 수 있다
+
+*/
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d"); // 도구 선택 getContext
+/* dpr 높을수록 더 선명하다 */
+const dpr = window.devicePixelRatio;
+/*
+캠퍼스 사이즈를 조절하는 방식
+1. 직접 css로 작업
+2. method 이용
+*/
+
+const canvasWidth = innerWidth; // 전체화면으로 초기화
+const canvasHeight = innerHeight;
+
+canvas.style.width = canvasWidth + "px";
+canvas.style.height = canvasHeight + "px";
+
+canvas.width = canvasWidth * dpr;
+canvas.height = canvasHeight * dpr;
+ctx.scale(dpr, dpr);
+
+// 사각형
+// ctx.fillRect(10, 10, 50, 50);
+// fillRect(시작하는 x위치, 시작하는 y위치, 가로길이, 세로길이) 사각형
+
+class Particle {
+  // particle을 class instance로 생성
+  constructor(x, y, radius, vy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.vy = vy;
+  }
+  update() {
+    this.y += this.vy;
+  }
+  draw() {
+    // 원
+    ctx.beginPath(); // 경로 그리기 시작한다고 알려줘야함
+    ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360);
+    // fillRect(시작하는 x위치, 시작하는 y위치, 반지름길이, 시작하는 각도, 끝나는 각도, 시계방향일지 반시계방향일지) 원
+    // ctx.stroke(); //선만 준다
+    ctx.fillStyle = "orange";
+    ctx.fill(); //섹싱을 준다
+    ctx.closePath(); // 끝냈다고 알려줘야함
+  }
+}
+
+const x = 100;
+const y = 100;
+const radius = 50;
+
+const particle = new Particle(x, y, radius);
+const TOTAL = 20;
+const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
+};
+
+let particles = [];
+for (let i = 0; i < TOTAL; i++) {
+  const x = randomNumBetween(0, canvasWidth);
+  const y = randomNumBetween(0, canvasHeight);
+  const radius = randomNumBetween(50, 100);
+  const vy = randomNumBetween(1, 5);
+  const particle = new Particle(x, y, radius, vy);
+  particles.push(particle);
+}
+
+let interval = 1000 / 60; // 60 fps를 타겟으로
+let now, delta;
+let then = Date.now();
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  now = Date.now();
+  delta = now - then;
+
+  if (delta < interval) return;
+
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  // clearRect(시작하는 x위치, 시작하는 y위치, width, height) => 매 순번마다 지우고 다시그리기를 반복
+  particles.forEach((particle) => {
+    particle.update();
+    particle.draw();
+    if (particle.y - particle.radius > canvasHeight) {
+      particle.y = -particle.radius;
+      particle.x = randomNumBetween(0, canvasWidth);
+      particle.radius = randomNumBetween(50, 100);
+      particle.vy = randomNumBetween(1, 5);
+    }
+  });
+  then - now - (delta % interval);
+}
+
+animate();
--- canvas/canvas draw particle base/index.js
+++ canvas/canvas draw particle base/index.js
@@ -19,9 +19,11 @@ canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
 // 사각형
-// ctx.fillRect(10, 10, 50, 50); // fillRect(시작하는 x위치, 시작하는 y위치, 가로길이, 세로길이) 사각형
+// ctx.fillRect(10, 10, 50, 50);
+// fillRect(시작하는 x위치, 시작하는 y위치, 가로길이, 세로길이) 사각형
 
 class Particle {
+  // particle을 class instance로 생성
   constructor(x, y, radius) {
     this.x = x;
     this.y = y;
@@ -30,9 +32,11 @@ class Particle {
   draw() {
     // 원
     ctx.beginPath(); // 경로 그리기 시작한다고 알려줘야함
-    ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360); // fillRect(시작하는 x위치, 시작하는 y위치, 반지름길이, 시작하는 각도, 끝나는 각도, 시계방향일지 반시계방향일지) 원
+    ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360);
+    // fillRect(시작하는 x위치, 시작하는 y위치, 반지름길이, 시작하는 각도, 끝나는 각도, 시계방향일지 반시계방향일지) 원
+    // ctx.stroke(); //선만 준다
     ctx.fillStyle = "red";
-    ctx.fill();
+    ctx.fill(); //섹싱을 준다
     ctx.closePath(); // 끝냈다고 알려줘야함
   }
 }
@@ -48,7 +52,7 @@ function animate() {
   window.requestAnimationFrame(animate);
 
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
+  // clearRect(시작하는 x위치, 시작하는 y위치, width, height) => 매 순번마다 지우고 다시그리기를 반복
   particle.draw();
 }
 
